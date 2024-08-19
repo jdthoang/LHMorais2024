68403552119f67f1430d35238a0fb1c8cfcac61e
Jonathan Hoang <113149010+jdthoang@users.noreply.github.com>
2024-08-19 12:52:51 -0700

    Transcriptomics analysis and plots

---
+++ Cell type deconvolution by DWLS 2024_08_19.R
@@ -0,0 +1,57 @@
+#################################################################################
+#################################################################################
+### Cell Type Composition Deconvolution Using Dampened Weighted Least Squares ###
+#################################################################################
+#################################################################################
+
+# load required packages
+library(DWLS)
+library(Seurat)
+
+# load sc data and metadata
+sc_path = 'path to scRNAseq matrix files'
+data = ReadMtx(mtx = paste0(scpath, '/matrix.mtx'),
+               cells = paste0(scpath, './barcodes.tsv'),
+               features = paste0(scpath, './genes.tsv'))
+data = as.matrix(data)
+metadata = read.csv('metadata.csv')
+
+# create cell type signatures
+label = as.vector(metadata$cell_type)
+signature = buildSignatureMatrixMAST(scdata = data,
+                                     id = label,
+                                     path = './DWLS.results/',
+                                     diff.cutoff = 0.5,
+                                     pval.cutoff = 0.01)
+save(signature,file="DWLS.RData")
+
+signature = MASTSignatureMatrixGivenDE(scdata = data,
+                                       id = labels,
+                                       path = 'DWLS.results',
+                                       diff.cutoff = 0.5,
+                                       pval.cutoff = 0.01)
+save(signature,file="./DWLS.results/DWLS.RData")
+
+# read in count matrix for deconvolution
+COUNTS = read.csv("count_matrix.tsv", header = T)
+CPM = sweep(COUNTS,2,as.numeric(colSums(COUNTS)),FUN="/")*1000000
+
+# filter counts for genes in signature matrix
+CPM.trim = CPM[rownames(signature),]
+samples = colnames(CPM.trim)
+
+# Initializing Results Data Frames for Each Algorithm that DWLS employs
+DWLS = data.frame()
+
+# Computing Cell Fractions for Each Sample and Adding to Results Data Frames
+for(sample in samples){
+  bulk = CPM.trim[,sample]
+  names(bulk) = rownames(CPM.trim)
+  tr=trimData(signature,bulk)
+  solDWLS=solveDampenedWLS(tr$sig,tr$bulk)
+  DWLS = rbind(DWLS,solDWLS)}
+model = list(DWLS,SVR,OLS)
+
+# Adding the rownames and columns back to each dataframe in list
+L = lapply(model, function(df){colnames(df) = colnames(signature); rownames(df) = samples; df})
+write.csv(L[1],file="CellFractions_PFCref_DWLS.csv")
---
+++ Top 25 DEGs heatmap 2024_08_19.R
@@ -0,0 +1,88 @@
+###################################################################
+###################################################################
+### Generate Scaled Heatmap of Top 25 DEGs for Each Comparison ###
+###################################################################
+###################################################################
+
+# load required packages
+library(DESeq2)
+library(ComplexHeatmap)
+library(viridis)
+
+# set WD
+path = "path"; setwd(path)
+
+# read in data
+COUNTS = read.table("./gene_counts_striatum.tsv", sep = '\t')
+CPM = sweep(COUNTS,2,as.numeric(colSums(COUNTS)),FUN="/")*1000000
+
+metadata = read.csv("./metadata_striatum.csv", row.names = 'X')
+
+# diffential gene expression with DESeq2
+dds = DESeq(DESeqDataSetFromMatrix(countData=COUNTS, 
+                             colData=metadata, 
+                             design=~condition, tidy = F))
+
+# set conditions to compare
+comp = c('ASO_GF','WT_GF')
+comp = rbind(comp,c('ASO_SPF', 'WT_SPF'))
+comp = rbind(comp,c('WT_GF', 'WT_SPF'))
+comp = rbind(comp,c('ASO_GF', 'ASO_SPF'))
+             
+# create heatmaps of top 25 DEG
+for(i in 1:4){
+  con1 = comp[i,1]; con2 = comp[i,2]; logfcthr = 0.5
+  res = results(dds, contrast = c('condition',con1, con2), pAdjustMethod ='BH'); res = res[order(res$padj),]
+  topDEGs = res[(!is.na(res$padj)),] #res$padj < 0.05 & 
+  topDEGs = topDEGs[order(topDEGs$log2FoldChange, decreasing = T),]
+  
+  # select genes to plot
+  sig_genes = c(head(rownames(topDEGs), 25),tail(rownames(topDEGs), 25))
+  
+  # convert to matrix and scale
+  mat = CPM[sig_genes, metadata$condition %in% c(con1, con2)]
+  heat = t(scale(t(mat)))
+  
+  # sort by metadata
+  metadata$Genotype = toupper(metadata$Genotype)
+  ann = data.frame(
+    Genotype = metadata$Genotype[metadata$condition %in% c(con1, con2)],
+    Microbiome = metadata$Microbiome[metadata$condition %in% c(con1, con2)],
+    stringsAsFactors = FALSE)
+  
+  # create the colour mapping
+  colours = list(Genotype = c('WT' = 'white', 'ASO' = 'black'), Microbiome = c('SPF' = 'gray', 'GF' = '#4682b4'))
+  
+  # create heatmap structure
+  colAnn = HeatmapAnnotation(
+    df = ann,
+    which = 'col',
+    na_col = 'white',
+    col = colours,
+    annotation_height = 0.6,
+    annotation_width = unit(1, 'cm'),
+    gap = unit(1, 'mm'),
+    border = T,
+    annotation_legend_param = list(
+      Genotype = list(
+        nrow = 2, title = 'Genotype', title_position = 'topcenter', legend_direction = 'vertical',
+        title_gp = gpar(fontsize = 12, fontface = 'bold'),
+        labels_gp = gpar(fontsize = 12, fontface = 'bold')),
+      Microbiome = list(
+        nrow = 2, title = 'Microbiome', title_position = 'topcenter', legend_direction = 'vertical',
+        title_gp = gpar(fontsize = 12, fontface = 'bold'),
+        labels_gp = gpar(fontsize = 12, fontface = 'bold'))))
+  
+  hmap = Heatmap(heat, cluster_rows = T, cluster_columns = F,
+                 top_annotation = colAnn, width = unit(0.3, "npc"), height = unit(0.75, "npc"),
+                 heatmap_legend_param = list(at = seq(-4, 4, 2)),
+                 rect_gp = gpar(col = "black"),
+                 col = viridis(100))
+  
+  # initialize SVG to save file
+  svg(file=paste0(con1, '_vs_', con2, '_top-25-DEG-heatmap.svg',sep = ""),
+      width = 3.47*3, height = 6.27*2)
+  draw(hmap, heatmap_legend_side = 'left', annotation_legend_side = 'right', row_sub_title_side = 'left',
+       padding = unit(c(0.5, 0.5, 0.5, 0.5), "in"))
+  dev.off()
+}
